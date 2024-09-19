ea4c3c35abfcbd1437a2f42707382a7b24f407fa
Matsko Danylo <danylogit@gmail.com>
2024-09-19 09:12:10 +0300

    What

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-Improve a landing page following the given design (same as
-previous) by adding full responsiveness and some animation
+Лабораторні роботи з дисципліни "Вебтехнології та вебдизайн"
+
+Виконав: Мацко Данило Любомирович (ІР-21)
+
+Лабораторна робота №1 (Варіант 7)
+
+Simple landing page
+
+Description: Create a landing page following the given design (Figma).
--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
-  function toggleMenu() {
-    const nav = document.querySelector(".header-line nav");
-    nav.classList.toggle("active");
-  }
-
-  // Assign the toggleMenu function to the menu icon click event
-  const menuIcon = document.querySelector(".menu-icon");
-  menuIcon.addEventListener("click", toggleMenu);
-});
+function toggleMenu() {
+  const nav = document.querySelector(".header-line nav");
+  nav.classList.toggle("active");
+}
